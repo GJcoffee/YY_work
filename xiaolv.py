95b95173eb72f12b16cd36a6a89141f6a29c78ff
HJ <1909621145@qq.com>
2023-03-09 15:14:58 +0800

    完成效率写入

--- xiaolv.py
+++ xiaolv.py
@@ -12,11 +12,10 @@ def pinyin(word):
     return s
 
 
-def read_efficiency(read_path, index_begin, index_end):
+def read_efficiency(read_path, index_end):
     """
     读取效率数据
     :param read_path: 文件路径
-    :param index_begin: 开始行
     :param index_end: 结束行
     :return: 姓名-效率
     """
@@ -44,39 +43,49 @@ def read_data(read_path, name_index, index_begin, index_end):
         name = name.title() if '）' not in name else name.split('（')[0].title()
         ret_dict[name] = float(v if v else "0") + float(w if w else "0") + float(x if x else "0") + float(
             y if y else "0")
-        # print(ret_dict)
+    print(ret_dict)
     return ret_dict
 
 
-def insert_data(insert_path, _dict):
+def insert_data(insert_path, holiday_dict, efficiency_dict):
     null_list = []
     name_list = []
     name_dict = {}
     wb = openpyxl.load_workbook(insert_path)
-    sheet = wb['效率总表2021-2022']
-
+    sheet = wb['效率总表2021-2023']
     for i in range(1, sheet.max_row):
-        sheet[f'A{i}'].value = sheet[f'A{i}'].value.repace(' ', '')
-        name_list.append(sheet[f'A{i}'].value)
-        name_dict[sheet[f'A{i}'].value] = i
-        # print(sheet[f'A{i}'].value)
-    for term in _dict:
-        if name_dict.get(term):
-            holiday = _dict[term] / 8
+        # 获取表格中的全部姓名并去掉空格
+        name = sheet[f'A{i}'].value
+        name = name if not name else name.replace(' ', '').title()
+        sheet[f'A{i}'].value = name
+        name_list.append(name)
+        name_dict[name] = i
+    print(name_list)
+    for term in holiday_dict:
+        # 遍历请假数据，导入请假和效率数据
+        if term in name_list:
+            holiday = holiday_dict[term] / 8
             print(holiday)
             if not holiday:
                 sheet[f'DA{name_dict[term]}'].value = None
+                sheet[f'CZ{name_dict[term]}'].value = efficiency_dict.get(f'{term}')
+                sheet[f'CY{name_dict[term]}'].value = '20'
+                continue
             else:
                 sheet[f'DA{name_dict[term]}'].value = 'Holiday=%2.3f' % holiday
-                sheet[f'CZ{name_dict[term]}'].value = ''
+                sheet[f'CZ{name_dict[term]}'].value = efficiency_dict.get(f'{term}')
+                sheet[f'CY{name_dict[term]}'].value = '20'
+                continue
         else:
-            null_list.append({term: _dict[term]})
-    print('未导入数据：', name_list)
+            null_list.append({term: holiday_dict[term]})
+    print('未导入数据：')
+    for x in null_list:
+        print(x)
     wb.save(rf'{os.path.expanduser("~")}\Desktop\Montly Efficiency_Color4Games2.xlsx')
 
 
 if __name__ == '__main__':
-    path = rf'{os.path.expanduser("~")}\Desktop\好了的.xls'
-    # list_test = read_data(r'C:\Users\86131\Desktop\成都麟动科技有限公司_月度汇总_20230201-202302281.xlsx', 'A', 5, 66)
-    read_efficiency(path, 1, 51)
-    # insert_data(r'C:\Users\86131\Desktop\Montly Efficiency_Color4Games.xlsx', list_test)
+    path = rf'{os.path.expanduser("~")}\Desktop'
+    holiday_dict = read_data(os.path.join(path, '成都麟动科技有限公司_月度汇总_20230201-202302281.xlsx'), 'A', 5, 66)
+    efficiency_dict = read_efficiency(os.path.join(path, '好了的.xls'), 51)
+    insert_data(rf'{path}\Montly Efficiency_Color4Games.xlsx', holiday_dict, efficiency_dict)
